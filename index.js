a04a68fb7fba81fda8e481cefb2045fa9e075091
Nomad24 <nassipolla@outlook.com>
2020-04-21 14:12:52 +0300

    minor changes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 require("dotenv/config");
 
 //Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
 
